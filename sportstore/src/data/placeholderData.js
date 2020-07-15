d82c6d48380db55092bad7be624829facea7124e
Marcin Polonis <marcin.polonis@o2.pl>
2020-07-15 17:08:16 +0200

    Add placeholderData.js, Types.js, ActionCreators.js, ShopReducer.js, DataStore.js, ProductList.js, CategoryNavigation.js into project

---
+++ sportstore/src/data/placeholderData.js
@@ -0,0 +1,25 @@
+export const data = {
+    categories: ["Sporty wodne", "Piłka nożna", "Wspinaczka"],
+    products: [
+        {
+            id: 1, name: "P1", category: "Sporty wodne",
+            description: "P1 (Sporty wodne)", price: 3
+        },
+        {
+            id: 2, name: "P2", category: "Sporty wodne",
+            description: "P2 (Sporty wodne)", price: 4
+        },
+        {
+            id: 3, name: "P3", category: "Piłka nożna",
+            description: "P3 (Piłka nożna)", price: 5
+        },
+        {
+            id: 4, name: "P4", category: "Wspinaczka",
+            description: "P4 (Wspinaczka)", price: 6
+        },
+        {
+            id: 5, name: "P5", category: "Wspinaczka",
+            description: "P5 (Wspinaczka)", price: 7
+        },
+    ]
+}
