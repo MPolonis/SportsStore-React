aad3a97fca9b6855a56d0d14820c1066830038f8
Marcin Polonis <marcin.polonis@o2.pl>
2020-07-18 16:21:01 +0200

    Add function of pagination into shop project

--- sportstore/src/data/ActionCreators.js
+++ sportstore/src/data/ActionCreators.js
@@ -7,3 +7,13 @@ export const loadData = (dataType, params) => ({
     type: ActionTypes.DATA_lOAD,
     payload: dataSource.GetData(dataType, params).then(response => ({dataType, data: response.data, total: Number(response.headers["x-total-count"]), params}))
 });
+
+export const setPageSize = (newSize) => ({
+    type: ActionTypes.DATA_SET_PAGESIZE, 
+    payload: newSize
+});
+
+export const setSortProperty = (newProp) => ({
+    type: ActionTypes.DATA_SET_SORT_PROPERTY,
+    payload: newProp
+});
